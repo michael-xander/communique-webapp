944e9b2fe583023c4f3ff68eba73dd21fccedb00
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-09-19 22:10:51 +0200

    Added tests for Patient upload form

--- patients/tests/test_forms.py
+++ patients/tests/test_forms.py
@@ -4,6 +4,8 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 import csv, os
 
 from patients.forms import PatientUploadFileForm
+from patients.models import Patient
+
 from communique.base_settings import BASE_DIR
 
 
@@ -12,11 +14,18 @@ class PatientUploadFileFormTestCase(TestCase):
     Test cases for the form handling uploading patient information.
     """
     file_path = BASE_DIR + '/patients/tests/data/'
+    test_files = [
+        file_path + 'correct.csv',
+        file_path + 'missing-column.csv',
+        file_path + 'switched-columns.csv',
+        file_path + 'missing-field.csv',
+        file_path + 'existing-patient.csv'
+    ]
 
     def setUp(self):
         # set up files to be used in testing
         # the expected file format i.e the file is delimited with ';'
-        with open(self.file_path + 'upload.csv', 'w', encoding='utf-8') as csvfile:
+        with open(self.test_files[0], 'w', encoding='utf-8') as csvfile:
             fieldnames = ['patient_id', 'last_name', 'names', 'health_centre', 'dob', 'address', 'treatment_start_date',
                           'interim_outcome']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
@@ -25,9 +34,53 @@ class PatientUploadFileFormTestCase(TestCase):
                              'health_centre':"St. Michael's Hospital", 'dob':'01/01/2001', 'address':'Long Street',
                              'treatment_start_date':'01/01/2001', 'interim_outcome':'Clinic treatment' })
 
+        # a file with a missing column
+        with open(self.test_files[1], 'w', encoding='utf-8') as csvfile:
+            fieldnames = ['patient_id', 'last_name', 'names', 'health_centre', 'dob', 'address', 'treatment_start_date']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writeheader()
+
+            writer.writerow({'patient_id': 'A001', 'last_name': 'Snow', 'names': 'Jon',
+                             'health_centre': "St. Michael's Hospital", 'dob': '01/01/2001', 'address': 'Long Street',
+                             'treatment_start_date': '01/01/2001'})
+
+        # a file with switched columns
+        with open(self.test_files[2], 'w', encoding='utf-8') as csvfile:
+            fieldnames = ['last_name', 'patient_id', 'names', 'health_centre', 'dob', 'address', 'treatment_start_date',
+                          'interim_outcome']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writeheader()
+
+            writer.writerow({'patient_id': 'A001', 'last_name': 'Snow', 'names': 'Jon',
+                            'health_centre': "St. Michael's Hospital", 'dob': '01/01/2001', 'address': 'Long Street',
+                            'treatment_start_date': '01/01/2001'})
+
+        # a file with one of the required fields missing
+        with open(self.test_files[3], 'w', encoding='utf-8') as csvfile:
+            fieldnames = ['patient_id', 'last_name', 'names', 'health_centre', 'dob', 'address', 'treatment_start_date',
+                          'interim_outcome']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writeheader()
+
+            writer.writerow({'last_name': 'Snow', 'names': 'Jon','health_centre': "St. Michael's Hospital",
+                             'dob': '01/01/2001', 'address': 'Long Street','treatment_start_date': '01/01/2001'})
+
+        Patient.objects.create(other_names='Jon', last_name='Snow', sex=Patient.MALE, identifier='A002')
+
+        # a file containing an ID for an existing patient
+        with open(self.test_files[4], 'w', encoding='utf-8') as csvfile:
+            fieldnames = ['patient_id', 'last_name', 'names', 'health_centre', 'dob', 'address', 'treatment_start_date',
+                          'interim_outcome']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writeheader()
+            writer.writerow({'patient_id': 'A002', 'last_name': 'Snow', 'names': 'Jon',
+                             'health_centre': "St. Michael's Hospital", 'dob': '01/01/2001', 'address': 'Long Street',
+                             'treatment_start_date': '01/01/2001', 'interim_outcome': 'Clinic treatment'})
+
     def tearDown(self):
         # delete the created files
-        os.remove(self.file_path + 'upload.csv')
+        for file_name in self.test_files:
+            os.remove(file_name)
 
     def test_validation(self):
         """
@@ -37,9 +90,41 @@ class PatientUploadFileFormTestCase(TestCase):
         self.assertFalse(form.is_bound)
         self.assertFalse(form.is_valid())
 
-        with open(self.file_path + 'upload.csv', 'rb') as csvfile:
-            file_data = {'uploaded_file': SimpleUploadedFile('upload.csv', csvfile.read(), content_type='text/plain')}
+        with open(self.test_files[0], 'rb') as csvfile:
+            file_data = {'uploaded_file': SimpleUploadedFile('file.csv', csvfile.read(), content_type='text/plain')}
 
         form = PatientUploadFileForm({}, file_data)
         self.assertTrue(form.is_bound)
         self.assertTrue(form.is_valid())
+
+        with open(self.test_files[1], 'rb') as csvfile:
+            file_data = {'uploaded_file':SimpleUploadedFile('file.csv', csvfile.read(),
+                                                            content_type='text/plain')}
+
+        form = PatientUploadFileForm({}, file_data)
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())
+
+        with open(self.test_files[2], 'rb') as csvfile:
+            file_data = {'uploaded_file': SimpleUploadedFile('file.csv', csvfile.read(),
+                                                             content_type='text/plain')}
+
+        form = PatientUploadFileForm({}, file_data)
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())
+
+        with open(self.test_files[3], 'rb') as csvfile:
+            file_data = {'uploaded_file': SimpleUploadedFile('file.csv', csvfile.read(),
+                                                             content_type='text/plain')}
+
+        form = PatientUploadFileForm({}, file_data)
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())
+
+        with open(self.test_files[4], 'rb') as csvfile:
+            file_data = {'uploaded_file': SimpleUploadedFile('file.csv', csvfile.read(),
+                                                             content_type='text/plain')}
+
+        form = PatientUploadFileForm({}, file_data)
+        self.assertTrue(form.is_bound)
+        self.assertFalse(form.is_valid())
