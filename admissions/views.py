f8a8495e25fb9e300c922cfa9f6a59af537b1655
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-10-14 19:28:18 +0200

    Add capability to export all for admissions

--- admissions/views.py
+++ admissions/views.py
@@ -1,12 +1,14 @@
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse
 
-import csv
+import datetime
 
 from communique.views import (CommuniqueCreateView, CommuniqueDetailView, CommuniqueDeleteView, CommuniqueListView,
-                              CommuniqueUpdateView, CommuniqueExportFormView, CommuniqueExportListView)
+                              CommuniqueUpdateView, CommuniqueExportFormView, CommuniqueExportListView,
+                              DATE_FORMAT_STR, DATE_FORMAT, CommuniqueListAndExportView)
 from .models import Admission
 from .forms import AdmissionUpdateForm, AdmissionCreateForm
+from .utils.utils_views import write_admissions_to_csv
 
 
 class AdmissionCreateView(CommuniqueCreateView):
@@ -37,13 +39,23 @@ class AdmissionUpdateView(CommuniqueUpdateView):
     context_object_name = 'admission'
 
 
-class AdmissionListView(CommuniqueListView):
+class AdmissionListView(CommuniqueListAndExportView):
     """
     A view that lists the existing admissions.
     """
     model = Admission
     template_name = 'admissions/admission_list.html'
     context_object_name = 'admission_list'
+
+    def csv_export_response(self, context):
+        # generate a csv for exportation
+        today = datetime.date.today()
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="all_admissions_{0}.csv"'.format(
+            today.strftime(DATE_FORMAT))
+        write_admissions_to_csv(response, context[self.context_object_name], DATE_FORMAT, DATE_FORMAT_STR)
+
+        return response
 
 
 class AdmissionDeleteView(CommuniqueDeleteView):
@@ -85,31 +97,9 @@ class AdmissionExportListView(CommuniqueExportListView):
         # generate an HTTP response with the csv file for the download
         start_date = self.get_export_start_date()
         end_date = self.get_export_end_date()
-        date_format = '%d-%m-%Y'
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="admissions_{0}_to_{1}.csv"'.format(
-            start_date.strftime(date_format), end_date.strftime(date_format))
+            start_date.strftime(DATE_FORMAT), end_date.strftime(DATE_FORMAT))
+        write_admissions_to_csv(response, context[self.context_object_name], DATE_FORMAT, DATE_FORMAT_STR)
 
-        fieldnames = ['id', 'patient_id', 'admission_date (dd-mm-yyyy)', 'discharge_date (dd-mm-yyyy)', 'health_centre',
-                      'notes', 'created_by', 'date_created (dd-mm-yyyy)',  'last_modified_by',
-                      'date_last_modified (dd-mm-yyyy)']
-        writer = csv.DictWriter(response, fieldnames=fieldnames, delimiter=';')
-        writer.writeheader()
-
-        for admission in context[self.context_object_name]:
-            patient = admission.patient
-
-            # check if the discharge date has been modified
-            if admission.discharge_date:
-                discharge_date = admission.discharge_date.strftime(date_format)
-            else:
-                discharge_date = ''
-            writer.writerow({'id':admission.id, 'patient_id':patient.id,
-                             'admission_date (dd-mm-yyyy)':admission.admission_date.strftime(date_format),
-                             'discharge_date (dd-mm-yyyy)':discharge_date, 'health_centre':admission.health_centre,
-                             'notes':admission.notes, 'created_by':admission.created_by.get_full_name(),
-                             'date_created (dd-mm-yyyy)':admission.date_created.strftime(date_format),
-                             'last_modified_by':admission.last_modified_by.get_full_name(),
-                             'date_last_modified (dd-mm-yyyy)':admission.date_last_modified.strftime(date_format)
-                             })
         return response
