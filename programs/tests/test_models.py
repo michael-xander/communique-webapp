fdff50fa40b8de9b34827c906b6b002ff624e4c3
Michael Kyeyune <michaelkyeyune01@gmail.com>
2016-10-20 20:31:04 +0200

    Update __str__ method for program models

--- programs/tests/test_models.py
+++ programs/tests/test_models.py
@@ -9,32 +9,31 @@ class ProgramTestCase(TestCase):
     Test cases for the Program model.
     """
     def setUp(self):
-        Program.objects.create(name='sample Program', description='sample description')
+        self.program = Program.objects.create(name='Sample Program', description='sample description')
 
     def test_program_str(self):
         """
         A test case for the __str__ method of Program model.
         """
-        program = Program.objects.get(id=1)
-        self.assertEqual(program.__str__(), 'Sample Program')
+        self.assertEqual(self.program.__str__(), self.program.name)
 
     def test_get_absolute_url(self):
         """
         A test case for the get_absolute_url method of the Program model.
         """
-        program = Program.objects.get(id=1)
-        self.assertEqual(program.get_absolute_url(), reverse('programs_program_detail', kwargs={'pk':program.pk}))
+        self.assertEqual(self.program.get_absolute_url(), reverse('programs_program_detail',
+                                                                  kwargs={'pk':self.program.pk}))
 
     def test_get_update_url(self):
         """
         A test case for the get_update_url method of the Program model.
         """
-        program = Program.objects.get(id=1)
-        self.assertEqual(program.get_update_url(), reverse('programs_program_update', kwargs={'pk':program.pk}))
+        self.assertEqual(self.program.get_update_url(), reverse('programs_program_update',
+                                                                kwargs={'pk':self.program.pk}))
 
     def test_get_delete_url(self):
         """
         A test case for the get_delete_url method of the Program model.
         """
-        program = Program.objects.get(id=1)
-        self.assertEqual(program.get_delete_url(), reverse('programs_program_delete', kwargs={'pk':program.pk}))
+        self.assertEqual(self.program.get_delete_url(), reverse('programs_program_delete',
+                                                                kwargs={'pk':self.program.pk}))
