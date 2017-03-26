3824f7fdb6ad5aca0c531172d925699b4bf49d75
Michael Kyeyune <michaelkyeyune01@gmail.com>
2017-03-26 15:47:54 +0200

    Fixed application context
    Made the application context available

--- communique/wsgi.py
+++ communique/wsgi.py
@@ -9,5 +9,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
 # check for specified settings module (for prod settings) and utilise dev settings if not found
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "communique.prod_settings")
+
+application = get_wsgi_application()
