668b39bb155f3ac364ffedec9dd0979d4a3d7501
Michael Kyeyune <michaelkyeyune01@gmail.com>
2017-03-26 15:33:46 +0200

    Deleted Heroko configurations
    Deleted any configurations specifically set up for a deployment on a
    Heroic server

--- communique/prod_settings.py
+++ communique/prod_settings.py
@@ -25,7 +25,3 @@ DATABASES = {
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- communique/wsgi.py
+++ communique/wsgi.py
@@ -9,13 +9,5 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-# imports specific to heroku deployment
-from whitenoise.django import DjangoWhiteNoise
-
 # check for specified settings module (for prod settings) and utilise dev settings if not found
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "communique.dev_settings")
-
-application = get_wsgi_application()
-# command specific to Heroku
-application = DjangoWhiteNoise(application)
